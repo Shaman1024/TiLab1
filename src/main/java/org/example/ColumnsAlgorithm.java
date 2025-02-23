8ff9cfdbae6a823d9cc0d6a32faaa392f09c53c1
1024 <vanya10293847@gmail.com>
2025-02-24 01:07:06 +0300

    columns

--- src/main/java/org/example/ColumnsAlgorithm.java
+++ src/main/java/org/example/ColumnsAlgorithm.java
@@ -1,38 +1,67 @@
 package org.example;
 
 public class ColumnsAlgorithm {
-    String input;
-    String key;
 
-    public ColumnsAlgorithm(String input, String key) {
-        this.input = input;
-        this.key = key;
-    }
 
-    public String getInput() {
-        return input;
-    }
+    private char[][] sortMatrix(char[][] matrix, int rows, int columns) {
+        char temp = ' ';
+        for (int i = 0; i < columns; i++) {
+            for (int j  = 0; i < columns - i - 1; i++) {
+                if (matrix[0][j] > matrix[0][j + 1]) {
+                    for (int k = 0; k < rows; k++) {
+                        temp = matrix[k][j];
+                        matrix[k][j] = matrix[k + 1][j];
+                        matrix[k + 1][j] = temp;
+                    }
+                }
+            }
+        }
 
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
+        return matrix;
     }
 
     public String columnsAlgorithm(String input, String key) {
         Validation validation = new Validation();
         input = validation.validate(input);
         key = validation.validate(key);
+        int rows = input.length()/key.length() + 1;
+        int columns = key.length();
 
-        char[][] matrix = new char[input.length()/key.length() + 1][key.length()];
+        char[][] matrix = new char[rows][columns];
+        int counter = 0;
 
-        return null;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (counter >= input.length() - 1){
+                    matrix[i][j] = ' ';
+                    continue;
+                }
+                matrix[i][j] = input.charAt(counter);
+                counter++;
+            }
+        }
+
+        matrix = sortMatrix(matrix, rows, columns);
+
+        StringBuilder sb = new StringBuilder();
+        counter = 0;
+
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (matrix[j][i] == ' '){
+                    continue;
+                }
+                sb.append(matrix[j][i]);
+                counter++;
+                if (counter == 5) {
+                    sb.append(' ');
+                    counter = 0;
+                }
+            }
+        }
+        System.out.println(sb.toString());
+
+        return sb.toString();
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        ColumnsAlgorithm columnsAlgorithm = new ColumnsAlgorithm();
+        columnsAlgorithm.columnsAlgorithm("АБВГД ЕЁЖЗИ ЙКЛМН ОПРСТ УФХЦЧ ШЩ", "абАААААд");
         System.out.println("Hello, World!");
     }
 }
